d79aece279c30aac58e1b7a0c00e5b98885cce0a
Bayes <bayes0621@gmail.com>
2022-06-03 19:05:30 +0800

    feat: babel

---
+++ babel-demo/index.js
@@ -0,0 +1,2 @@
+const arr = new Array(5).fill(1)
+console.log("🚀 ~ file: index.js ~ line 2 ~ arr", arr)
---
+++ babel-demo/output/index.js
@@ -0,0 +1,4 @@
+"use strict";
+
+const arr = new Array(5).fill(1);
+console.log("🚀 ~ file: index.js ~ line 2 ~ arr", arr);
