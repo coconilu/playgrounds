bfd72ea00a226787f1821f8d207d553458f0d60d
Yaohui Wang <yaohui.wang@shopee.com>
2022-06-19 14:06:40 +0800

    feat: vital 时机

--- react-demo/src/reportWebVitals.js
+++ react-demo/src/reportWebVitals.js
@@ -1,10 +1,10 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry); // not for safari
+      getFID(onPerfEntry); // not for safari
       getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
+      getLCP(onPerfEntry); // not for safari
       getTTFB(onPerfEntry);
     });
   }
