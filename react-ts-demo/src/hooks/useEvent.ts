b5ded8e27c5021555a24e22f873825ceb04a3ab8
Yaohui Wang <yaohui.wang@shopee.com>
2022-07-06 18:23:29 +0800

    feat: useEvent

--- react-ts-demo/src/hooks/useEvent.ts
+++ react-ts-demo/src/hooks/useEvent.ts
@@ -1,12 +1,17 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useMemo, useRef } from "react";
 
-export const useEvent = <T extends (...args: any[]) => any>(func: T) => {
-  const funcRef = useRef<T>(func);
-  funcRef.current = func;
+export const useEvent = <T extends (...args: any[]) => any>(func?: T) => {
+  const funcRef = useRef<T | undefined>(func);
+  // why not write `fnRef.current = fn`?
+  // https://github.com/alibaba/hooks/issues/728
+  funcRef.current = useMemo(() => func, [func]);
 
-  const stableCallback = useCallback((...args: any[]) => {
-    return funcRef.current?.(...args);
-  }, []);
+  const stableCallback = useCallback<(...args: Parameters<T>) => ReturnType<T>>(
+    (...args) => {
+      return funcRef.current?.(...args);
+    },
+    []
+  );
 
   return stableCallback;
 };
