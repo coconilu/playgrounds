2e3ad579de7968b9f35e78913a69e0833a593f0a
Bayes <827130441@qq.com>
2022-12-07 13:50:15 +0800

    feat: ts ValueOf

--- types/utils.ts
+++ types/utils.ts
@@ -5,3 +5,10 @@ type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
   : false;
 
 type result = Equal<string | number, string>;
+
+type ValueOf<T> = T[keyof T];
+
+type result1 = ValueOf<{
+  a: string;
+  b: number;
+}>;
